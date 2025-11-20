2b8b6e8e49778f638e894f629677da023cfbddc9
coni-cp <c.parraguezs@duocuc.cl>
2025-11-20 13:43:27 -0300

    header

--- header.php
+++ header.php
@@ -10,17 +10,43 @@
 </head>
 
 <body>
-    <header class="header">
+   <header class="header">
         <div class="header-left d-flex justify-content-center">
             <i class="fa-brands fa-instagram fa-xl" style="color: #ffffff;"></i>
             <i class="fa-brands fa-facebook fa-xl ms-2" style="color: #ffffff;"></i>
         </div>
 
         <div class="header-right">
-            <i class="fa-solid fa-cart-shopping fa-xl" style="color: #ffffff;"></i>
+            <a href="carrito.html"><i class="fa-solid fa-cart-shopping fa-xl" style="color: #ffffff;"></i></a>
             <div class="search-container">
                 <input type="text" class="search-input" placeholder="Buscar...">
             </div>
             <i class="fa-solid fa-magnifying-glass fa-xl search-btn punto" style="color: #ffffff;"></i>
         </div>
     </header>
+    <nav class="navbar">
+        <div class="nav-left">
+            <a href="index.html"><img src="assets/img/logoheader.png" alt="Logo" class="logo"></a>
+        </div>
+
+        <div class="nav-right">
+             <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'menu-principal',
+                            'container' => false,
+                            'menu_class' => '',
+                            'fallback_cb' => '__return_false',
+                            'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                            'depth' => 2,
+                            'walker' => new bootstrap_5_wp_nav_menu_walker()
+                        ));
+                        ?>
+
+            <!--Menú hamburguesa-->
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </nav>
