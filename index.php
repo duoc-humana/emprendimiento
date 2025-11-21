44d7bf98673cda36664725f4408e0e43b1856760
Sayrinix <sa.luciano@duocuc.cl>
2025-11-21 17:02:18 -0300

    aaa

--- index.php
+++ index.php
@@ -5,7 +5,20 @@ get_header();
 
 <h1>Hola, este es mi tema desde cero</h1>
 
+<?php
+$query = new WP_Query(array(
+    'post_type' => 'empresas_home',
+    'posts_per_page' => -1
+));
 
+while ($query->have_posts()) : $query->the_post();
+    $fields = SCF::get('empresa_campos', get_the_ID());
+?>
+    <section>
+        <img src="<?php echo esc_url(wp_get_attachment_url($fields['empresa'])); ?>">
+    </section>
+
+<?php endwhile; wp_reset_postdata(); ?>
 
 <?php
 get_footer();
