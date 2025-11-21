5c999aca366beb91509c0c4982f85546d21ddf18
Sayrinix <sa.luciano@duocuc.cl>
2025-11-21 17:43:25 -0300

    Ahora si xfis

--- index.php
+++ index.php
@@ -8,28 +8,34 @@ get_header();
 
 
 <!-- Banner de empresas con swiper !-->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
 
-<div class="swiper mySwiper">
-  <div class="swiper-wrapper">
+            <?php
+                $query = new WP_Query(array(
+                    'post_type' => 'empresas_home',
+                    'posts_per_page' => -1
+                ));
 
-    <?php
-$query = new WP_Query(array(
-    'post_type' => 'empresas_home',
-    'posts_per_page' => -1
-));
+                while ($query->have_posts()) : $query->the_post();
+                    $fields = get_post_meta(get_the_ID(), 'empresa', true);
+                    $img_url = wp_get_attachment_url($fields);
 
-while ($query->have_posts()) : $query->the_post();
-    $fields = get_post_meta(get_the_ID(), 'empresa', true);
-    $img_url = wp_get_attachment_url($fields);
+            ?>
 
-?>
-
-    <div class="swiper-slide ajuste">
-        <img src="<?php echo esc_url($img_url); ?>">
+            <div class="swiper-slide ajuste">
+                <img src="<?php echo esc_url($img_url); ?>">
+            </div>
+            <?php endwhile; wp_reset_postdata(); ?>
+                </div>
+            </div>
+        </div>
     </div>
-    <?php endwhile; wp_reset_postdata(); ?>
-  </div>
 </div>
+
 
 
 
