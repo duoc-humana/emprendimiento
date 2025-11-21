ebeb8d7646424e803642258d7b527fa49922f584
Sayrinix <sa.luciano@duocuc.cl>
2025-11-21 16:39:14 -0300

    banner de empresas con swiper

--- index.php
+++ index.php
@@ -5,6 +5,25 @@ get_header();
 
 <h1>Hola, este es mi tema desde cero</h1>
 
+<!--Banner de empresas con swiper !--> 
+<div class="swiper mySwiper">
+  <div class="swiper-wrapper">
+    <?php
+    $query = new WP_Query(array(
+    'post_type' => 'empresas_home',
+    'posts_per_page' => -1
+    ));
+
+    while ($query->have_posts()) : $query->the_post();
+    $fields = SCF::get('empresas_home', get_the_ID());
+?>
+    <div class="swiper-slide"> 
+    <img src="<?php echo esc_url(wp_get_attachment_src($fields['empresa'])); ?>">
+    </div>
+<?php endwhile; wp_reset_postdata(); ?>
+  </div>
+</div>
+
 <?php
 get_footer();
 ?>
