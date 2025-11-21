d3f55aad841b94185e3d0ad9c5b7c54ba381ef5d
Sayrinix <sa.luciano@duocuc.cl>
2025-11-21 16:49:48 -0300

    a

--- index.php
+++ index.php
@@ -10,17 +10,22 @@ get_header();
   <div class="swiper-wrapper">
     <?php
     $query = new WP_Query(array(
-    'post_type' => 'empresas_home',
-    'posts_per_page' => -1
+        'post_type' => 'empresas_home',
+        'posts_per_page' => -1
     ));
 
     while ($query->have_posts()) : $query->the_post();
-    $fields = SCF::get('empresas_home', get_the_ID());
-?>
-    <div class="swiper-slide"> 
-    <img src="<?php echo esc_url(wp_get_attachment_image_src($fields['empresa'])); ?>">
-    </div>
-<?php endwhile; wp_reset_postdata(); ?>
+
+        // Obtener el campo (ID de la imagen)
+        $imagen_id = SCF::get('empresa', get_the_ID());
+
+        // Obtener la URL correcta
+        $img = wp_get_attachment_image_src($imagen_id, 'full');
+    ?>
+        <div class="swiper-slide">
+            <img src="<?php echo esc_url($img[0]); ?>" alt="">
+        </div>
+    <?php endwhile; wp_reset_postdata(); ?>
   </div>
 </div>
 
