bdb97aa1d2f22a0f3fc9cd172cc7179c75c6f59a
joji <jos.oyaneder@duocuc.cl>
2025-11-21 13:21:54 -0300

    a

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recicla2</title>
+
     <?php wp_head(); ?>
 </head>
 
