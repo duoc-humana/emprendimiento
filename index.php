93c3e2ca2ae967a782bd20a0ec12e33a71aa0388
Sayrinix <sa.luciano@duocuc.cl>
2025-11-21 18:24:06 -0300

    sss

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ get_header();
 
             ?>
 
-            <div class="swiper-slide col-3">
+            <div class="swiper-slide col-lg-2">
                 <img class="ajuste" src="<?php echo esc_url($img_url); ?>">
             </div>
             <?php endwhile; wp_reset_postdata(); ?>
