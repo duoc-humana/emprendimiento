c2543ced2eff72b13180c695e5fc905830b98894
Sayrinix <sa.luciano@duocuc.cl>
2025-11-21 18:40:12 -0300

    xdd

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ searchContainers.forEach((searchContainer) => {
 const swiper = new Swiper(".mySwiper", {
     loop: true,
     slidesPerView: 'auto',
-    slidesPerGroup: '4',
+    slidesPerGroup: '3',
     spaceBetween: 0,
     speed: 5000,
     freeMode: true,
