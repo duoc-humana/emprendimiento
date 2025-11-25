9b8fdaaa8146fabf510e2fe1cb2d201ac15bc7dc
coni-cp <c.parraguezs@duocuc.cl>
2025-11-25 11:00:06 -0300

    hshhsdj

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,17 +18,10 @@ searchContainers.forEach((searchContainer) => {
  var swiper = new Swiper(".mySwiper", {
     loop: true,
       spaceBetween: 30,
-      centeredSlides: true,
+       slidesPerView: 3,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
       },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+      
     });
