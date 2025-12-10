30d0f24fb9960212c7353c26cfc366f53c9bddb3
Sayrinix <sa.luciano@duocuc.cl>
2025-12-10 11:43:48 -0300

    contacto

--- page-contacto.php
+++ page-contacto.php
@@ -8,6 +8,19 @@ Template Name: Página de Contacto
 get_header();
 ?>
 
+<div class="container">
+    <div class="row">
+        <a href="<?php echo home_url(); ?>">Ir al home</a>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="row">
+                <img src="assets/img/icono-ubicacion.png" alt="">
+            </div>
+        </div>
+        <div class="col-6"></div>
+    </div>
+</div>
 
 <?php
 get_footer();
