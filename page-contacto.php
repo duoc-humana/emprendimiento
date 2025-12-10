8a91211d1e2ffe7260d6058455bf01edbc5d7b61
Sayrinix <sa.luciano@duocuc.cl>
2025-12-10 12:08:06 -0300

    Update page-contacto.php

--- page-contacto.php
+++ page-contacto.php
@@ -14,11 +14,13 @@ get_header();
     </div>
     <div class="row">
         <div class="col-6">
-            <div class="row caja-iconos-sy">
+            <div class="row caja-iconos-sy d-flex justify-content-center">
                 <div class="col-2">
                 <img class="img-iconos-sy" src="<?php echo get_template_directory_uri(); ?>/assets/img/icono-ubicacion.png" alt="">
                 </div>
-                
+                <div class="col-2">
+                    <h3>Dirección</h3>
+                </div>
             </div>
         </div>
         <div class="col-6"></div>
