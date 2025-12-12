126f6e961b3cd5d440638ea8fe4ede23fe8cd425
Sayrinix <sa.luciano@duocuc.cl>
2025-12-12 20:16:06 -0300

    Update page-gracias.php

--- page-gracias.php
+++ page-gracias.php
@@ -7,7 +7,7 @@ Template Name: Página de Gracias
 // Archivo base para cargar el tema.
 get_header();
 ?>
-<div class="container my-5 mx-5 d-flex justify-content-center">
+<div class="container my-5 mx-5 d-flex justify-content-center flex-column">
  <h1>Gracias por contactarnos! <i class="fa-solid fa-dove ms-2" style="color: #26364d;"></i></h1> 
  <a href="<?php echo home_url(); ?>" class="links-sy">Volver al home</a>  
 </div>
