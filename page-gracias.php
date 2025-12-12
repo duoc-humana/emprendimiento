2caf99e01860527bf7bde65db5c312be94924fdc
Sayrinix <sa.luciano@duocuc.cl>
2025-12-12 20:11:21 -0300

    a

--- page-gracias.php
+++ page-gracias.php
@@ -7,8 +7,10 @@ Template Name: Página de Gracias
 // Archivo base para cargar el tema.
 get_header();
 ?>
+<div class="container my-5 mx-5">
+ <h1>Gracias por contactarnos! <i class="fa-solid fa-dove ms-2" style="color: #26364d;"></i></h1>   
+</div>
 
-<h1>Gracias por contactarnos!</h1>
 
 
 <?php
