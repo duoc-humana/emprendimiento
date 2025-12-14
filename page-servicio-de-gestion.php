4df72147999a770585f35002cded5436c76982f1
Sayrinix <sa.luciano@duocuc.cl>
2025-12-14 14:44:06 -0300

    Update page-servicio-de-gestion.php

--- page-servicio-de-gestion.php
+++ page-servicio-de-gestion.php
@@ -111,13 +111,12 @@ get_header();
     <!-- Formulario ++++++ -->
      <div class="container-fluid esp3">
         <div class="row">
-            <div class="col-4 mx-0 px-0 img-fondo-tex-sy d-flex justify-content-center">
+            <div class="col-5 ms-0 me-5 px-0 img-fondo-tex-sy d-flex justify-content-center">
                 <div class="color-fondo-sy">
                   <h3 class="pb-3">Recuerda que:</h3>
                     <p class="text-start">La gestión del material textil puede realizarse tanto mediante retiro coordinado por nuestro equipo como por entrega directa por parte de la empresa. Una vez recibida esta solicitud, nos pondremos en contacto para definir la modalidad más adecuada, junto con los detalles de fecha, lugar y logística según el tipo y volumen de material.</p>  
                 </div> 
             </div>
-            <div class="col-1"></div>
             <div class="col-6">
                 <form action="POST">
                 <div class="row">
