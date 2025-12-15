c88fc8445b451ccaa080e1e2f1a05dbd691ab822
Sayrinix <sa.luciano@duocuc.cl>
2025-12-15 17:14:56 -0300

    Update page-contacto.php

--- page-contacto.php
+++ page-contacto.php
@@ -17,7 +17,7 @@ get_header();
     <div class="row g-5 my-5">
         <div class="col-12 col-lg-5 order-2 order-lg-1">
 
-            |<div class="info-card text-center p-4 mb-4">
+            <div class="info-card text-center p-4 mb-4">
                 <img class="icono-contacto mb-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/icono-ubicacion.png" alt="Icono ubicación">
                 <p class="fw-semibold mb-0">Dirección</p>
             </div>
@@ -29,23 +29,23 @@ get_header();
 
             <div class="info-card text-center p-4 mb-4">
                 <img class="icono-contacto mb-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/icono-mail.png" alt="Icono mail">
-                <a class="links-sy mb-0 fw-semibold" href="mailto:consuelo.miranda@gmail.com">consuelo.miranda@gmail.com</a>
+                <a class="links-sy mb-0 fw-semibold d-block" href="mailto:consuelo.miranda@gmail.com">consuelo.miranda@gmail.com</a>
             </div>
         </div>
         <div class="col-12 col-lg-7 order-1 order-lg-2">
-                <h1>Formulario de contacto</h1>
-                <p class="py-3 text-muted">No dudes en comunicarte con el equipo Recicla2, estaremos felices de resolver cualquier duda que tengas sobre nuestros productos, procesos y proyectos especiales</p>
-                <ul class="ps-5 text-muted">
-                    <li class="fw-light">Diferentes medios de pago</li>
-                    <li class="fw-light">Plazo de respuesta de 1 a 3 días hábiles.</li>
-                </ul>
+            <h1>Formulario de contacto</h1>
+            <p class="py-3 text-muted">No dudes en comunicarte con el equipo Recicla2, estaremos felices de resolver cualquier duda que tengas sobre nuestros productos, procesos y proyectos especiales</p>
+            <ul class="ps-5 text-muted">
+                <li class="fw-light">Diferentes medios de pago</li>
+                <li class="fw-light">Plazo de respuesta de 1 a 3 días hábiles.</li>
+            </ul>
 
-                <form action="<?php echo admin_url('admin-post.php'); ?>" method="POST">
-                    <input type="hidden" name="action" value="enviar_contacto">
+            <form action="<?php echo admin_url('admin-post.php'); ?>" method="POST">
+                <input type="hidden" name="action" value="enviar_contacto">
 
-                    <?php wp_nonce_field('form_contacto', 'nonce_contacto'); ?>
+                <?php wp_nonce_field('form_contacto', 'nonce_contacto'); ?>
                     
-                    <div class="row my-3 g-3">
+                <div class="row my-3 g-3">
                     <div class="col-md-6">
                         <label for="nombre">Nombre y Apellido</label>
                         <input id="nombre" name="nombre" class="form-control" type="text">  
@@ -54,21 +54,22 @@ get_header();
                         <label for="telefono">Teléfono</label>
                         <input type="tel" class="form-control" name="telefono" id="telefono">
                     </div>
-                        <div class="col-12">
-                            <label for="mail">Correo electrónico</label>
-                            <input type="email" class="form-control" name="email" id="mail">
-                        </div>
-                        <div class="col-12">
-                            <label for="asunto">Asunto</label>
-                            <input name="asunto" id="asunto" class="form-control" type="text">
-                        </div>
-                        <div class="col-12">
-                            <label for="mensaje">Escribe tu mensaje</label>
-                            <textarea name="mensaje" class="form-control" id="mensaje"></textarea>
-                        </div>
+                    <div class="col-12">
+                        <label for="mail">Correo electrónico</label>
+                        <input type="email" class="form-control" name="email" id="mail">
+                    </div>
+                    <div class="col-12">
+                        <label for="asunto">Asunto</label>
+                        <input name="asunto" id="asunto" class="form-control" type="text">
+                    </div>
+                    <div class="col-12">
+                        <label for="mensaje">Escribe tu mensaje</label>
+                        <textarea name="mensaje" class="form-control" id="mensaje"></textarea>
+                    </div>
                     <div class="col-12 d-flex justify-content-end mb-5 mt-3">
                         <button type="submit" class="boton-enviar-sy">Enviar</button>
-                    </div>     
+                    </div> 
+                </div>    
             </form>
         </div>
     </div>
